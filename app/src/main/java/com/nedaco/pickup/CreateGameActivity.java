0ae9455f13e80801e03a8ab99a159488d15de3af
pdanylewycz <danylewycz.2@osu.edu>
2015-10-27 13:24:49 -0400

    Create Game Activity Added location services

--- app/src/main/java/com/nedaco/pickup/CreateGameActivity.java
+++ app/src/main/java/com/nedaco/pickup/CreateGameActivity.java
@@ -1,23 +1,83 @@
 package com.nedaco.pickup;
 
 import android.content.Intent;
+import android.graphics.Point;
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.parse.ParseGeoPoint;
+import com.parse.ParseObject;
 
 import java.util.prefs.Preferences;
 
-public class CreateGameActivity extends AppCompatActivity {
+public class CreateGameActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,  com.google.android.gms.location.LocationListener {
+
+    private Button mCreateGame;
+    private Spinner spinner1, spinner2, spinner3, spinner4;
+    private Location mLastLocation;
+    private TextView mLatitudeText, mLongitudeText;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+
+        Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+        if (location == null) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+
+        } else {
+        mLastLocation = location;
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d("Connection suspened", "Connection suspended");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.d("Connection suspened", "Connection suspended");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_game);
 
-        Spinner spinner1 = (Spinner) findViewById(R.id.spin_sport);
+        buildGoogleApiClient();
+
+        // create LocationRequest
+        mLocationRequest = LocationRequest.create().setPriority(LocationRequest.PRIORITY_LOW_POWER)
+                .setInterval(60 * 1000).setFastestInterval(10 * 1000);
+
+        spinner1 = (Spinner) findViewById(R.id.spin_sport);
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
                 R.array.sport_spinner, android.R.layout.simple_spinner_item);
@@ -26,7 +86,7 @@ public class CreateGameActivity extends AppCompatActivity {
         // Apply the adapter to the spinner
         spinner1.setAdapter(adapter1);
 
-        Spinner spinner2 = (Spinner) findViewById(R.id.spin_numplayers);
+        spinner2 = (Spinner) findViewById(R.id.spin_numplayers);
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
                 R.array.numplayers_spinner, android.R.layout.simple_spinner_item);
@@ -35,7 +95,7 @@ public class CreateGameActivity extends AppCompatActivity {
         // Apply the adapter to the spinner
         spinner2.setAdapter(adapter2);
 
-        Spinner spinner3 = (Spinner) findViewById(R.id.spin_location);
+        spinner3 = (Spinner) findViewById(R.id.spin_location);
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this,
                 R.array.location_spinner, android.R.layout.simple_spinner_item);
@@ -44,7 +104,7 @@ public class CreateGameActivity extends AppCompatActivity {
         // Apply the adapter to the spinner
         spinner3.setAdapter(adapter3);
 
-        Spinner spinner4 = (Spinner) findViewById(R.id.spin_time);
+        spinner4 = (Spinner) findViewById(R.id.spin_time);
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter4 = ArrayAdapter.createFromResource(this,
                 R.array.time_spinner, android.R.layout.simple_spinner_item);
@@ -52,6 +112,14 @@ public class CreateGameActivity extends AppCompatActivity {
         adapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         spinner4.setAdapter(adapter4);
+
+        mCreateGame = (Button) findViewById(R.id.button_createGame);
+        mCreateGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                storeGame();
+            }
+        });
 
     }
 
@@ -71,16 +139,15 @@ public class CreateGameActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        switch (id)
-        {
+        switch (id) {
             case R.id.map:
-                intent = new Intent(CreateGameActivity.this,MapsActivity.class);
+                intent = new Intent(CreateGameActivity.this, MapsActivity.class);
                 break;
             //case R.id.create_game:
-             //   intent = new Intent(CreateGameActivity.this,CreateGameActivity.class);
-              //  break;
+            //   intent = new Intent(CreateGameActivity.this,CreateGameActivity.class);
+            //  break;
             case R.id.Preferences:
-                intent = new Intent(CreateGameActivity.this,PreferencesActivity.class);
+                intent = new Intent(CreateGameActivity.this, PreferencesActivity.class);
                 break;
             default:
                 intent = null;
@@ -88,14 +155,56 @@ public class CreateGameActivity extends AppCompatActivity {
 
         }
 
-        if(intent!=null)
-        {
+        if (intent != null) {
             startActivity(intent);
         }
         //if (id == R.id.map) {
-         //   return true;
-       // }
+        //   return true;
+        // }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    public void storeGame() {
+        ParseGeoPoint point = new ParseGeoPoint(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+        ParseObject game = new ParseObject("Game");
+
+        game.put("location", point);
+        game.put("sport", spinner1.getSelectedItem().toString());
+        game.put("time", spinner4.getSelectedItem().toString());
+        game.put("number_of_players", spinner2.getSelectedItem());
+        game.saveInBackground();
+        Toast.makeText(getApplicationContext(), "Game Saved", Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("Creategameactivity", "Activity onPause() called.");
+        // don't want to continue using api client when the app is not in the foreground, so disconnect
+        if (mGoogleApiClient.isConnected()) mGoogleApiClient.disconnect();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("Creategameactivity", "Activity onResume() called.");
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        handleNewLocation(location);
+    }
+
+    private void handleNewLocation(Location location) {
+        Log.d("Creategameactivity", location.toString());
+
+        mLongitudeText.setText(String.valueOf(location.getLongitude()));
+        mLatitudeText.setText(String.valueOf(location.getLatitude()));
+
+
     }
 }
